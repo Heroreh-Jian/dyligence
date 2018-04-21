54ddbd19e47301ff260aeeccf889cdf6f83011f0
Jian Yu <Jian@Jians-MacBook-Air.local>
2018-04-20 17:55:25 -0700

    dyligence

---
+++ js/config.js
@@ -0,0 +1,23 @@
+/******************************************************************************************************************/
+/* VIRGILIO | Responsive Personal Portfolio Theme                                                                 */
+/* (c) 2017 Rene Puchinger                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["Human-led. AI-powered."];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'particle-fountain', 'video'  */
+var config_frontPageEffect = 'particles';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+
+/* Google map parameters */
+var config_mapLat = 37.3978888; // latitude
+var config_mapLng =  -121.98460749999998; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
